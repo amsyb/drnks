6b398ce1cbacdf38931bc1a0db6f18129bdf3a51
annasybingco <sybingcoanna@gmail.com>
2025-10-29 15:44:40 -0700

    import font families, set background color, set text color, import react router, began homepage files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Homepage.scss";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>hello there</h1>
+      <p>
+        Transforming mixology from mystery to mastery. The essential digital
+        companion for professionals and enthusiasts alike, providing precise
+        recipes and techniques when you need them most.
+      </p>
+    </div>
+  );
+}
+
+export default Homepage;
