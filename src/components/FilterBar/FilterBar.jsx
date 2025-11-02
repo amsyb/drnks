1d13151743f87bf394aaa55dd76b639628c84ff7
annasybingco <sybingcoanna@gmail.com>
2025-11-01 20:07:28 -0700

    update filter functionality, filter content is now displayed by default, made filter responsive, update button sizing

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import FilterIcon from "../../assets/icons/filter.svg";
 import "./FilterBar.scss";
 
 function FilterBar({ filters, setFilters }) {
-  const [isOpen, setIsOpen] = useState(false);
-
   const baseSpirits = [
     "All",
     "Gin",
@@ -34,59 +30,40 @@ function FilterBar({ filters, setFilters }) {
 
   return (
     <div className="filter-bar">
-      <button
-        className={`filter-bar__toggle ${hasActiveFilters ? "active" : ""}`}
-        onClick={() => setIsOpen(!isOpen)}
-        aria-label="Toggle filters"
-      >
-        <img src={FilterIcon} alt="" className="filter-bar__icon" />
-        <span className="filter-bar__text">Filters</span>
-        {hasActiveFilters && <span className="filter-bar__badge"></span>}
-      </button>
-
-      {isOpen && (
-        <div className="filter-bar__content">
-          <div className="filter-bar__section">
-            <label className="filter-bar__label">Base Spirit:</label>
-            <div className="filter-bar__buttons">
-              {baseSpirits.map((spirit) => (
-                <button
-                  key={spirit}
-                  className={`filter-bar__button ${
-                    filters.baseSpirit === spirit ? "active" : ""
-                  }`}
-                  onClick={() => handleSpiritChange(spirit)}
-                >
-                  {spirit}
-                </button>
-              ))}
-            </div>
+      <div className="filter-bar__content">
+        <div className="filter-bar__scroll-container">
+          <div className="filter-bar__buttons">
+            {baseSpirits.map((spirit) => (
+              <button
+                key={spirit}
+                className={`filter-bar__button ${
+                  filters.baseSpirit === spirit ? "active" : ""
+                }`}
+                onClick={() => handleSpiritChange(spirit)}
+              >
+                {spirit}
+              </button>
+            ))}
+            {methods.slice(1).map((method) => (
+              <button
+                key={method}
+                className={`filter-bar__button ${
+                  filters.method === method ? "active" : ""
+                }`}
+                onClick={() => handleMethodChange(method)}
+              >
+                {method}
+              </button>
+            ))}
           </div>
-
-          <div className="filter-bar__section">
-            <label className="filter-bar__label">Method:</label>
-            <div className="filter-bar__buttons">
-              {methods.map((method) => (
-                <button
-                  key={method}
-                  className={`filter-bar__button ${
-                    filters.method === method ? "active" : ""
-                  }`}
-                  onClick={() => handleMethodChange(method)}
-                >
-                  {method}
-                </button>
-              ))}
-            </div>
-          </div>
-
-          {hasActiveFilters && (
-            <button className="filter-bar__clear" onClick={clearFilters}>
-              Clear All Filters
-            </button>
-          )}
         </div>
-      )}
+
+        {hasActiveFilters && (
+          <button className="filter-bar__clear" onClick={clearFilters}>
+            Clear All Filters
+          </button>
+        )}
+      </div>
     </div>
   );
 }
